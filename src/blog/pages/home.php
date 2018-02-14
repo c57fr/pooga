922ef0cffbbac10773f24fa6af5e8ddaf6af97bb
COTE <GrCOTE7@GMail.com>
2018-02-14 04:42:07 +0100

    Restructuration des fichiers avec ob_start()

--- src/blog/pages/home.php
+++ src/blog/pages/home.php
@@ -1,4 +1,4 @@
 <div class="container">
-	<h1>Home</h1>
+	<h1>Home Page</h1>
 	<a href="index.php?p=single">Single Page</a>
 </div>
--- src/blog/public/index.php
+++ src/blog/public/index.php
@@ -4,12 +4,14 @@ const BASE_DIR = './src/blog/';
 
 $p = $_GET[ 'p' ] ?? 'home';
 
-var_dump( '$p', $p );
-//echo $p;
+//var_dump( $_GET );
 
+ob_start();
 if ( $p === 'home' ) {
 	require BASE_DIR . 'pages/home.php';
 }
 elseif ( $p === 'single' ) {
 	require BASE_DIR . 'pages/single.php';
 }
+$content = ob_get_clean();
+require BASE_DIR . 'pages/template/default.php';
