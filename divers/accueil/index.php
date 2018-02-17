0f2059ac2ee6465a8e9a55613b6424c6ac927e85
COTE <GrCOTE7@GMail.com>
2018-02-17 20:28:34 +0100

    Up lien

--- divers/accueil/index.php
+++ divers/accueil/index.php
@@ -7,10 +7,12 @@ $title = 'Accueil';
 	<ul>
 		<li><a href="https://www.grafikart.fr/formations/programmation-objet-php/" target="_blank">La POO en PHP - Tuto de
 				référence (GrafikArt)</a></li>
-		<li><p><a href="https://github.com/c57fr/pooga" target="_blank">Dépôt GitHub</a></p>
+		<li><p><a href="https://github.com/c57fr/pooga/tree/ch14" target="_blank">Dépôt GitHub</a></p>
 			<ul>
-				<li><a href="https://github.com/c57fr/pooga/blob/master/README.md" target="_blank">Guide d'installation rapide</a></li>
-				<li><a href="https://github.com/c57fr/pooga/issues/new" target="_blank">Aide</a></li></ul>
+				<li><a href="https://github.com/c57fr/pooga/blob/ch14/README.md" target="_blank">Guide d'installation rapide</a>
+				</li>
+				<li><a href="https://github.com/c57fr/pooga/issues/new" target="_blank">Aide</a></li>
+			</ul>
 		</li>
 	</ul>
 </h3>
--- divers/demoFactory/index.php
+++ divers/demoFactory/index.php
@@ -1,5 +1,3 @@
 <?php namespace Gc7\Divers\DemoFactory;
 
-$title = "DemoFactory";
-
 var_dump( CarFactory::create( 'BerLiNe' ) );
