3021c737bf66e29fc3704b65e7bb2fab3cbaddd7
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-06 19:20:13 +0100

    DIC Conteneur d'injections de dépendances

---
+++ DPC/5 DIC/DIC.php
@@ -0,0 +1,36 @@
+<?php namespace DIC;
+
+
+class DIC {
+
+	private $instances = [ ];
+	private $regitry   = [ ];
+	private $factories = [ ];
+
+
+	public function __construct () {
+	}
+
+
+	public function set ( $key, Callable $resolver ) {
+		$this->regitry[ $key ] = $resolver;
+	}
+
+	public function setFactory ( $key, Callable $resolver ) {
+		$this->factories[ $key ] = $resolver;
+	}
+
+	public function get ( $key ) {
+
+		if ( isset( $this->factories[ $key ] ) ) {
+			return $this->factories[ $key ]();
+		}
+
+		if ( ! isset( $this->instances[ $key ] ) ) {
+			$this->instances[ $key ] = $this->regitry[ $key ]();
+		}
+
+		return $this->instances[ $key ];
+	}
+
+}
