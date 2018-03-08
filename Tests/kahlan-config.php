c5842a1eb8228405ad30b78fe569bbce275a2736
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-08 21:00:46 +0100

    Déplacement de tout ce qui est tests Kahlan dans Tests/

---
+++ Tests/kahlan-config.php
@@ -0,0 +1,12 @@
+<?php
+use Kahlan\Filter\Filters;
+
+$commandLine = $this->commandLine();
+$commandLine->option('spec', 'default', 'Tests/spec');
+$commandLine->option('src', 'default', 'Tests/1_Demo');
+$commandLine->option('reporter', 'default', 'verbose');
+
+Filters::apply($this, 'namespaces', function($next) {
+	$this->autoloader()->addPsr4('Test\\Demo\\', dirname(__DIR__) . '/Tests/1_Demo/');
+	return $next();
+});
