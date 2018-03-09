1aa2312716e713e95efbe4ca37257ba725987802
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-09 22:38:08 +0100

    Add coverage Kahlan

--- Tests/kahlan-config.php
+++ Tests/kahlan-config.php
@@ -4,6 +4,7 @@ use Kahlan\Filter\Filters;
 $commandLine = $this->commandLine();
 $commandLine->option('spec', 'default', 'Tests/accueil/spec');
 $commandLine->option('src', 'default', 'Tests/1_Demo');
+$commandLine->option('coverage', 'default', 4);
 $commandLine->option('reporter', 'default', 'verbose');
 
 Filters::apply($this, 'namespaces', function($next) {
