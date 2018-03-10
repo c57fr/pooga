6191a30acdc9a70ea3d5e90f364bbaeb73cfd5ce
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-10 19:34:22 +0100

    Simplification du test

--- DPCpnt/6_Observer/index.php
+++ DPCpnt/6_Observer/index.php
@@ -2,7 +2,7 @@
 <h1 style="margin-top: -5px"><!--30--><a href=https://github.com/design-patterns-for-humans/French#-observer"
                                 target="_blank">Design Pattern Observer (Event)</a></h1>
 	
-	<a href="/?p=6 Observer&pg=indexga.php">Event</a> -
+	<a href="/?p=6_Observer&pg=indexga.php">Event</a> -
 	<a href="https://www.grafikart.fr/formations/programmation-objet-php/event-listener" target="_blank">Vu /
 	GA</a> (Events & listenners)
 <hr>
--- Tests/accueil/spec/5_EventGASpec.php
+++ Tests/accueil/spec/5_EventGASpec.php
@@ -9,12 +9,10 @@ describe( \Event\Emitter::class, function () {
 
 	it( 'should be a singleton', function () {
 
-		$instance1 = Emitter::getInstance();
-		$instance2 = Emitter::getInstance();
+		$instance = Emitter::getInstance();
 
-		expect( $instance1 )->toBeAnInstanceOf( Emitter::class );
-		expect( $instance2 )->toBeAnInstanceOf( Emitter::class );
-		expect( $instance1 )->toBe( $instance2 );
+		expect( $instance )->toBeAnInstanceOf( Emitter::class );
+		expect( $instance )->toBe( Emitter::getInstance());
 
 	} );
 
