47889515d0b4d7f154336f7b7f717412ac2d6963
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-10 23:07:07 +0100

    Dé pipé designé

--- GArtiens/douxlapinblanc/index.php
+++ GArtiens/douxlapinblanc/index.php
@@ -1,28 +1,36 @@
 <h1>Accueil <?= ucfirst( $_GET[ 'p' ] ) ?></h1>
 <?php
+use  Gc7\Helper\GC7Tip;
+
 
 $nbLances = 1e5;
 
-$pFace = range( 1, 10 );
+$faces = array_fill( 0, 6, 0 );
 
-$s = range( 1, 7 );
+$pFace       = range( 0, 9 );
+$tiragePiped = function ( $pFace ) {
+	$face = array_rand( $pFace );
+	$face = ( $face < 5 ) ? $face : 5;
 
-$tirage = function ( $pFace ) {
-	$res = array_rand( $pFace );
-	$res = $res < 5 ? $res : 5;
-
-	return $res;
+	return $face;
 };
 
 for ( $i = 0; $i < $nbLances; $i ++ ) {
-	$res = $tirage( $pFace );
-	$s[ $res ] ++;
+	$tiree = $tiragePiped( $pFace );
+	$faces[ $tiree ] ++;
 }
 
-$somme = 0;
-foreach ( $s as $k => $v ) {
-	$somme += $v;
-	echo $k . ' : ' . ( $v / $nbLances ) . ' <br>';
-}
 
-echo '<hr>' . Gc7\Helper\GC7Tip::nf( $i, 0 ) . ' lancés';
+echo '<br><p>' . GC7Tip::nf( $i, 0 ) . ' lancés d\'un dé pipé (La face 6 est "allourdie" 5 fois)</p><br>';
+
+echo '<table class="table table-bordered thead-dark table-striped">
+<thead class="table-dark" style="text-align: center"><th>Face</th><th>Sorties</th><th>%</th></thead>';
+
+foreach ( $faces as $k => $v ) {
+	echo '<tr style="text-align: right; padding-right: 200px;"><td style="text-align: right; padding-right: 15%">
+' . ( $k + 1 ) . '</td><td style="text-align: right; padding-right: 15%">' . Gc7Tip::nf( $v, 0 ) . '</td><td
+style="text-align: right; padding-right: 15%">
+' . Gc7Tip::nf( $v * 1e2 / $nbLances ) . '</td></tr>';
+}
+echo '</table>';
+
