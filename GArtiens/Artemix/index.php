466200e42a3310beae611ae4190431b5deb61f14
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-13 18:51:50 +0100

    Add PDF des SPECS

--- GArtiens/Artemix/index.php
+++ GArtiens/Artemix/index.php
@@ -1,2 +1,6 @@
 <h1>Accueil Artemix</h1>
-<a href="#" download="/?p=Artemix&pg=modele_dossier_de_specification.doc">Doc SPECS</a>  
+<a href="modele_dossier_de_specification.doc" download=<?$_DIR__"modele_dossier_de_specification.doc">Doc SPECS</a>  
+
+<hr>
+
+Oki 21azfggfdsdds
