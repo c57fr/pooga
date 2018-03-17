e2b92244a63b12bff644ae39a054c89ae128aeb3
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-17 03:39:21 +0100

    Rep via session

--- GArtiens/Momo/index.php
+++ GArtiens/Momo/index.php
@@ -1,14 +1,17 @@
 <h1>Accueil Momo</h1>
 <hr>
 <?php
+session_start();
+$_SESSION[ 'id' ] = 1;
 
+$bdd = new PDO( 'mysql:host=localhost;dbname=pooga', 'root', '' );
+$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+$bdd->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ );
 
-$bdd = new PDO("mysql:host=localhost;dbname=pooga", "root", "");
-$bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$bdd -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-
-$req = $bdd->prepare("SELECT id, username, password FROM users where id=?");
-$req->execute([1]);
+$req = $bdd->prepare( 'SELECT id, username, password FROM users WHERE id=?' );
+$req->execute( [ htmlspecialchars( $_SESSION[ 'id' ] ) ] );
 $rep = $req->fetch();
 
 var_dump($rep);
+
+echo ucfirst( $rep->username );
