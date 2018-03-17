8e75bfc98ed8b02a479072649bf684ad6889b0a5
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-17 07:03:13 +0100

    Req simplifiée

--- GArtiens/Momo/index.php
+++ GArtiens/Momo/index.php
@@ -1,4 +1,4 @@
-<h1>Accueil Momo</h1>
+<h1 style="margin-top: 30px">Accueil Momo</h1>
 <hr>
 <?php
 session_start();
@@ -12,6 +12,11 @@ $req = $bdd->prepare( 'SELECT id, username, password FROM users WHERE id=?' );
 $req->execute( [ htmlspecialchars( $_SESSION[ 'id' ] ) ] );
 $rep = $req->fetch();
 
-var_dump($rep);
+var_dump( $rep );
 
 echo ucfirst( $rep->username );
+
+$req2 = $bdd->query( 'SELECT id, username, password FROM users WHERE id=1' );
+$rep2 = $req2->fetch();
+
+var_dump( $rep2 );
