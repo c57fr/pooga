0448fcb3c2161ad4b0ac5c8be44934739f37c42a
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-17 03:16:10 +0100

    Req préparée

--- GArtiens/Momo/index.php
+++ GArtiens/Momo/index.php
@@ -7,8 +7,8 @@ $bdd = new PDO("mysql:host=localhost;dbname=pooga", "root", "");
 $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $bdd -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-$req = $bdd->prepare("SELECT id, username, password FROM users");
-$req->execute();
-$rep = $req->fetchAll();
+$req = $bdd->prepare("SELECT id, username, password FROM users where id=?");
+$req->execute([1]);
+$rep = $req->fetch();
 
 var_dump($rep);
