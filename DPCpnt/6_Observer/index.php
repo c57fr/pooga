0faef8e1998afc69c579d2ea2e6a8be80c63890d
GrCOTE7 <GrCOTE7@GMail.com>
2018-03-20 04:14:39 +0100

    Add EventManager

--- DPCpnt/6_Observer/index.php
+++ DPCpnt/6_Observer/index.php
@@ -1,11 +1,19 @@
 <?php namespace Observer\GA; ?>
 <h1 style="margin-top: -5px"><!--30--><a href=https://github.com/design-patterns-for-humans/French#-observer"
                                 target="_blank">Design Pattern Observer (Event)</a></h1>
-	
-	<a href="/?p=6_Observer&pg=indexga.php">Event</a> -
-	<a href="https://www.grafikart.fr/formations/programmation-objet-php/event-listener" target="_blank">Vu /
-	GA</a> (Events & listenners)
-<hr>
+
+Vu / GA :<br>
+<ul>
+<li>
+
+	<a href="/?p=6_Observer&pg=indexga1.php">Event 1</a> -
+	<a href="https://www.grafikart.fr/formations/programmation-objet-php/event-listener" target="_blank"> (Events & listenners)</a>
+</li>
+	<li>
+	<a href="/?p=6_Observer&pg=indexga2.php">Event 2</a> -
+	<a href="https://www.grafikart.fr/formations/programmation-objet-php/event-manager" target="_blank"> (Event Manager)</a>
+	</li>
+</ul>
 <?php
 //require dirname( __DIR__ ) . '/autoloader.php';
 require dirname( dirname( __DIR__ ) ) . '/vendor/autoload.php';
