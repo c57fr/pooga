27c894e4be59315cbf44460d56a6e3a935b909b7
Momo <seckndanane16@gmail.com>
2018-12-04 05:11:46 +0100

    exemple avec mon professeur

---
+++ Momo/accueil/index.php
@@ -0,0 +1,16 @@
+<h1>Accueil <?= ucfirst( \AutoMenu\AutoMenu::getAppName() ); ?></h1>
+<p>Test 123</em></p>
+
+<pre>
+
+Bin en ffait, là, ça marche nickel :-)
+- tu es le meilleur 
+- Bah... habitude, c tout... Et sinon, donc, tu as fini les vidéos de la série K ?
+- ouij'ai fini toutes les videos. Ce qui me bloqué c'est que je faisais sudo /opt/lampp/manager ...
+et ça ne marche pas 
+- Oui, je vois... Bah normalement en ligne de commande, ça marche toujours :-)
+
+-attends je te montre. je faisais composer install update et il y avait des lignes encadrées en jaune qui apparaissaient comme sur la photo à droite
+- ok hello
+
+- un exemple avec mon professeur
